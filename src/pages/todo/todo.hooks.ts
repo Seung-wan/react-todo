fc6da9dc09dfd3c8abb211e38d68f2b45975f932
Seung-wan <tmddhks0104@gmail.com>
2024-01-02 02:37:10 +0900

    feat(step2): step2를 개발합니다.

--- src/pages/todo/todo.hooks.ts
+++ src/pages/todo/todo.hooks.ts
@@ -1,14 +1,41 @@
 import { useCallback, useState } from 'react';
 
-export default function useTodo() {
-  const [todos, setTodos] = useState<string[]>([]);
+import { ALERT_MESSAGE } from '@/constants';
 
-  const addTodo = useCallback((todo: string) => {
-    setTodos((prev) => [...prev, todo]);
+import { Todo } from './types';
+
+let id = 1;
+
+export default function useTodo() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = useCallback(
+    (content: string) => {
+      // 중복 체크를 하는 조건이 다른 곳에서 사용 되지 않는다면? 2개의 함수가 필요해진다.
+      // addTodo라는 함수에 중복 추가를 금지하는 조건이 들어간다면 모든 곳에 동일하게 적용되는 것인가?
+      if (todos.some((todo) => todo.content === content)) {
+        return window.alert(ALERT_MESSAGE.DUPLICATED_TODO);
+      }
+
+      const newTodo = {
+        id,
+        content,
+      };
+
+      setTodos((prev) => [...prev, newTodo]);
+
+      id += 1;
+    },
+    [todos]
+  );
+
+  const deleteTodo = useCallback((id: number) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
   }, []);
 
   return {
     todos,
     addTodo,
+    deleteTodo,
   };
 }
--- src/pages/todo/todo.tsx
+++ src/pages/todo/todo.tsx
@@ -3,10 +3,15 @@ import { Spacing } from '@/components/common/spacing';
 import { TodoForm, TodoList } from './components';
 
 import useTodo from './todo.hooks';
+
 import './todo.css';
 
 export default function Todo() {
-  const { todos, addTodo } = useTodo();
+  const { todos, addTodo, deleteTodo } = useTodo();
+
+  const handleClickDelete = (id: number) => {
+    deleteTodo(id);
+  };
 
   return (
     <div className="todo-container">
@@ -14,7 +19,7 @@ export default function Todo() {
       <Spacing size={16} />
       <TodoForm addTodo={addTodo} />
       <Spacing size={16} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onClickDelete={handleClickDelete} />
     </div>
   );
 }
